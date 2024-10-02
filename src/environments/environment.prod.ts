3531c946cac1de9287d620d63007c054a9828254
aminata054 <tinaami054@gmail.com>
2024-10-02 13:09:37 +0000

    update 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDvzlZSyVdSzosU75iQ4X84SUo5ugBwLf0",
+    authDomain: "projetatc-fbb4e.firebaseapp.com",
+    projectId: "projetatc-fbb4e",
+    storageBucket: "projetatc-fbb4e.appspot.com",
+    messagingSenderId: "641575618026",
+    appId: "1:641575618026:web:691d6e6209a439bc089e93"
+  }
 };
